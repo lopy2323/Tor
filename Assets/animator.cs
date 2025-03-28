1b217678d466144dd8b0626ab2840bf17ce16827
lopy2323 <r.keeren@hotmail.com>
2025-03-28 12:08:29 +0100

    robert

--- Assets/animator.cs
+++ Assets/animator.cs
@@ -4,19 +4,61 @@ using UnityEngine;
 
 public class animator : MonoBehaviour
 {
+    [SerializeField] private float speed = 50f;
+    [SerializeField] private float rotSpeed = 50f;
     private Animator animations;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         animations = GetComponent<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        if (Input.GetKeyUp(KeyCode.T))
         {
-            animations.SetTrigger("triggers");
+            animations.SetTrigger("time");
+        };
+        if (Input.GetKeyDown(KeyCode.W)) 
+        { 
+            animations.SetBool("walk",true);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animations.SetBool("walk",false);
+        };
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animations.SetBool("back", true);
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animations.SetBool("back", false);
+        }
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            rb.AddForce(transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            Quaternion rot = transform.rotation;
+            rot = Quaternion.Euler(rot.eulerAngles + (new Vector3(0, rotSpeed, 0) * Time.deltaTime));
+            rb.MoveRotation(rot);
+            //  transform.rotation = rot;
+        }
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            Quaternion rot = transform.rotation;
+            rot = Quaternion.Euler(rot.eulerAngles + (new Vector3(0, -rotSpeed, 0) * Time.deltaTime));
+            rb.MoveRotation(rot);
+            //  transform.rotation = rot;
+        }
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            rb.AddForce(transform.forward * -speed * Time.deltaTime);
         };
     }
 }
